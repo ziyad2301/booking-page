c7376a5a7bce5aad23c769064275ec84c4e6bc2a
Mohammed Ziyad <ziyad.szakeer@gmail.com>
2023-08-31 16:22:09 +0530

    main changes

--- bookingpg1/settings.py
+++ bookingpg1/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'authentication',
     'intro',
     'reviews',
-    # 'whitenoise.runserver_nostatic'
+     'whitenoise.runserver_nostatic'
 ]
 
 MIDDLEWARE = [
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'bookingpg1.urls'
